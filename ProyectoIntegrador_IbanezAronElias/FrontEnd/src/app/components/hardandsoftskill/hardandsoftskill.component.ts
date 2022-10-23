edcf6ccc2e2e9a437fae3c97ce7e7bb5a4ca49db
Aron Elias Ibañez <70858294+Frenezyzz@users.noreply.github.com>
2022-10-23 17:03:25 -0300

    Creacion de carpeta FrontEnd
    creacion de la carpeta para dividir el Front del Back

---
+++ ProyectoIntegrador_IbanezAronElias/FrontEnd/src/app/components/hardandsoftskill/hardandsoftskill.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hardandsoftskill',
+  templateUrl: './hardandsoftskill.component.html',
+  styleUrls: ['./hardandsoftskill.component.css']
+})
+export class HardandsoftskillComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
